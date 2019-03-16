8fe5247e76350115735ad66763831f7077a10402
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-16 07:31:54 -0400

    Start mergesort method

--- Merge.java
+++ Merge.java
@@ -1,4 +1,17 @@
 
 public class Merge {
+  public static void mergesort(int[] data) {
+    mergesort(data, 0, data.length - 1);
+  }
+  private static void mergesort(int[] data, int lo, int hi) {
+    if (lo < hi) {
+      int half = (lo + hi) / 2
+      mergesort(lo, half);
+      mergesort(half + 1, hi);
+      merge(lo, half, hi);
+    }
+  }
+  private static int[] merge(int lo1, int lo2, int hi) {
 
+  }
 }
